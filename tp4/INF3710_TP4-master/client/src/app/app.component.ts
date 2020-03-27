570cc328ec093cacc9c968afb27472177f34be76
Hugo Perronnet <hugo.perronnet@polymtl.ca>
2020-03-27 16:35:05 -0400

    changed title

--- tp4/INF3710_TP4-master/client/src/app/app.component.ts
+++ tp4/INF3710_TP4-master/client/src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
           });
     }
 
-    public readonly title: string = "INF3710 TP5";
+    public readonly title: string = "INF3710 TP4";
     public ngOnInit(): void {
     }
 }
